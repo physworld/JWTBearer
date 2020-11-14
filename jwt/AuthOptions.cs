eb7b06e5673611147c428ded94ebe5512043e867
physworld <vladtkalenko@gmail.com>
2020-11-14 04:02:59 +0200

    nitial commit

---
+++ jwt/AuthOptions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jwt
+{
+    public class AuthOptions
+    {
+        public const string ISSUER = "MyAuthServer";    // издатель токена
+        public const string AUDIENCE = "MyAuthClient";  // потребитель токена
+        const string KEY = "secretsecret_key43256";
+        public const int LIFETIME = 60 * 24 * 30;
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
+        }
+    }
+}
